47fea280a58308414367a0d93adf7871404427c7
DWAB Cicconi <danielc@tradecoffeeco.com>
2020-12-28 19:38:25 -0500

    First push build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './scss/App.scss'
+import { EVIDENCE, GHOST_TYPES } from './constants'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      activeEvidence: [],
+      lookupGhost: ''
+    }
+
+    this._addActiveEvidence = this._addActiveEvidence.bind(this)
+    this._removeActiveEvidence = this._removeActiveEvidence.bind(this)
+  }
+
+  _removeActiveEvidence(evId) {
+    // console.log('_removeActiveEvidence', evId)
+    let newArray = [].concat(this.state.activeEvidence)
+
+    const evIds = newArray.filter( id => {
+      return id !== evId
+    })
+
+    this.setState({ activeEvidence: evIds })
+  }
+
+  _addActiveEvidence(evId) {
+    if (this.state.activeEvidence.length >= 3) {
+      console.log('Unclick another piece of evidence first')
+    } else {
+      let newArray = [].concat(this.state.activeEvidence)
+
+      newArray.push(evId)
+
+      let evIds = newArray.filter((num, i) => {
+        return newArray.indexOf(num) === i;
+      });
+
+      this.setState({ activeEvidence: evIds })
+    }
+  }
+
+  _handleGhostButtonClick(ghost) {
+    this.setState({ lookupGhost: ghost })
+  }
+
+  _handleEvidenceClick(evId) {
+    // console.log( 'clicked evidence', evId )
+    const evidenceIsCurrentlyActive = this.state.activeEvidence.indexOf(evId) !== -1
+
+    if (evidenceIsCurrentlyActive) {
+      this._removeActiveEvidence(evId)
+    } else {
+      this._addActiveEvidence(evId)
+    }
+  }
+
+  _renderEvidenceButtons() {
+    return EVIDENCE.map( button => {
+      const activeEvidenceFull = this.state.activeEvidence.length >= 3
+      const isActiveEvidence = this.state.activeEvidence.indexOf(button.id) !== -1
+
+      const isDisabled = !isActiveEvidence && activeEvidenceFull
+
+      const evidenceClasses = [
+        'evidence__button',
+        isActiveEvidence ? 'evidence__button--active' : ''
+      ].join(' ')
+
+      return (
+        <button key={`evidence${button.id}`} className={evidenceClasses} type="button" disabled={isDisabled} onClick={ () => { this._handleEvidenceClick(button.id) }}>
+          { button.name }
+        </button>
+      )
+    })
+  }
+
+  _renderPossibleGhosts() {
+    const { activeEvidence } = this.state
+    let possibleGhosts = GHOST_TYPES
+
+    if (activeEvidence.length > 0) {
+
+      possibleGhosts = GHOST_TYPES.filter( ghost => {
+        let notThisGhost = false
+
+        activeEvidence.forEach( pieceOfEvidence => {
+          const requiredEvidence = ghost.evidence
+          if ( requiredEvidence.indexOf(pieceOfEvidence) === -1 ) {
+            notThisGhost = true
+          }
+        })
+
+        /** Remove ghost from list of possible culprits */
+        if (notThisGhost) return false
+        return true
+      })
+    }
+
+    if (possibleGhosts.length <= 0) {
+      return <div className="ghost">Something is wrong. Double check your evidence again...</div>
+    }
+
+    return possibleGhosts.map( ghost => {
+      return (
+        <button key={`ghosts${ghost.id}`} className="ghost__button" onClick={() => { this._handleGhostButtonClick(ghost) }}>
+          { ghost.name }
+        </button>
+      )
+    })
+  }
+
+  _renderGhostDetails() {
+    const { lookupGhost } = this.state
+
+    return (
+      <div className="ghost-details__wrapper" onClick={ () => { this.setState({ lookupGhost: '' }) }}>
+        <h2>{ lookupGhost.name }</h2>
+        <ul>
+        {
+          lookupGhost.evidence.map( evidenceId => {
+            const item = EVIDENCE.find( element => { return element.id === evidenceId })
+
+            if (item) return <li>{item.name}</li>
+          })
+        }
+        </ul>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="evidence__wrapper">
+          { this._renderEvidenceButtons() }
+        </div>
+        { this.state.lookupGhost && this._renderGhostDetails() }
+        <div className="ghost__wrapper">
+          { this._renderPossibleGhosts() }
+        </div>
+        <footer>
+          Ghost Journal Companion App
+        </footer>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/constants.js
@@ -0,0 +1,89 @@
+export const GHOST_TYPES = [
+  {
+    id: 1,
+    name: 'Banshee',
+    evidence: [1, 2, 3],
+  },
+  {
+    id: 2,
+    name: 'Demon',
+    evidence: [3, 5, 6],
+  },
+  {
+    id: 3,
+    name: 'Jinn',
+    evidence: [1, 4, 6],
+  },
+  {
+    id: 4,
+    name: 'Mare',
+    evidence: [3, 4, 6],
+  },
+  {
+    id: 5,
+    name: 'Oni',
+    evidence: [1, 5, 6],
+  },
+  {
+    id: 6,
+    name: 'Phantom',
+    evidence: [1, 3, 4],
+  },
+  {
+    id: 7,
+    name: 'Poltergeist',
+    evidence: [2, 4, 6],
+  },
+  {
+    id: 8,
+    name: 'Revenant',
+    evidence: [1, 2, 5],
+  },
+  {
+    id: 9,
+    name: 'Shade',
+    evidence: [1, 4, 5],
+  },
+  {
+    id: 10,
+    name: 'Spirit',
+    evidence: [2, 5, 6],
+  },
+  {
+    id: 11,
+    name: 'Wraith',
+    evidence: [2, 3, 6],
+  },
+  {
+    id: 12,
+    name: 'Yurei',
+    evidence: [3, 4, 5],
+  }
+]
+
+export const EVIDENCE = [
+  {
+    id: 1,
+    name: 'EMF Level 5',
+  },
+  {
+    id: 2,
+    name: 'Fingerprints'
+  },
+  {
+    id: 3,
+    name: 'Freezing Temps'
+  },
+  {
+    id: 4,
+    name: 'Ghost Orb'
+  },
+  {
+    id: 5,
+    name: 'Ghost Writing'
+  },
+  {
+    id: 6,
+    name: 'Spirit Box'
+  }
+]
